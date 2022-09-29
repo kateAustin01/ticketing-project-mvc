1c4a008365b90e2df427e7752482f5f24a2147a5
aissakhova@gmail.com <aktoty@live.com>
2022-09-29 11:31:32 -0500

    Service impl classes added

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -1,8 +1,19 @@
 package com.cydeo.entity;
 
 import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public class User extends BaseEntity{
+import java.time.LocalDateTime;
+
+/*
+@Data doesn't look at other classes.
+They don't know about your superclass's non-zero-arg constructor. That is why if you are extending another
+class you need to create a constructor yourself,@Data doesn't work in this situation
+*/
+@NoArgsConstructor
+@Data
+public class User extends BaseEntity {
     private String firstName;
     private String lastName;
     private String userName;
@@ -11,4 +22,18 @@ public class User extends BaseEntity{
     private String phone;
     private Role role;
     private Gender gender;
+
+    public User(Long ID, LocalDateTime insertDateTime, Long insertUserID, LocalDateTime lastUpdateDateTime, Long lastUpdateUserID, String firstName, String lastName, String userName, String password, boolean enabled, String phone, Role role, Gender gender) {
+        super(ID, insertDateTime, insertUserID, lastUpdateDateTime, lastUpdateUserID);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.enabled = enabled;
+        this.phone = phone;
+        this.role = role;
+        this.gender = gender;
+    }
+
+
 }
